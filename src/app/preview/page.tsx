4fa746178c30ae9535c32ada957699bf66171694
Shivam Pandey <shivamkonan143@gmail.com>
2025-10-14 22:52:36 +0000

    Thambnail preview ke upar me enter video URL and get thambnail bhi show

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -4,7 +4,7 @@
 import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { Header } from '@/app/components/header';
-import { YoutubeDownloaderPreview } from '@/app/components/video-downloader';
+import { YoutubeDownloaderInput, YoutubeDownloaderPreview } from '@/app/components/video-downloader';
 import { Card, CardContent } from '@/components/ui/card';
 import { AlertCircle } from 'lucide-react';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
@@ -35,6 +35,7 @@ export default function PreviewPage() {
     <main className="flex min-h-screen w-full flex-col items-center bg-background px-4 pb-12 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(223,200,242,0.3),rgba(255,255,255,0))]">
       <Header />
       <div className="w-full max-w-2xl space-y-6">
+        <YoutubeDownloaderInput />
         <Suspense fallback={
             <Card>
                 <CardContent className="pt-6">
