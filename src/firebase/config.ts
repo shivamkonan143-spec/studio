2055965613916eed8b099f09e638b8c2fad67d86
Shivam Pandey <shivamkonan143@gmail.com>
2025-10-14 13:15:54 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "gen-lang-client-0584941007",
-  "appId": "1:587532559747:web:e025a0e13774b16debc6da",
-  "apiKey": "AIzaSyBj1CCC88QJKRdtnloDLiKl78nrWq2IIRI",
-  "authDomain": "gen-lang-client-0584941007.firebaseapp.com",
-  "storageBucket": "gen-lang-client-0584941007.appspot.com",
-  "messagingSenderId": "587532559747"
+  "projectId": "studio-2008852960-a9318",
+  "appId": "1:808392146991:web:e025a0e13774b16debc6da",
+  "apiKey": "AIzaSyAv-6b6y4qIILixgJhh1zoimOMc78M2RFk",
+  "authDomain": "studio-2008852960-a9318.firebaseapp.com",
+  "storageBucket": "studio-2008852960-a9318.appspot.com",
+  "messagingSenderId": "808392146991"
 };
