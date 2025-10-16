558529091604b719a4c79867962dae9283bbf944
Shivam Pandey <shivamkonan143@gmail.com>
2025-10-16 14:25:59 +0000

    apiKey: "AIzaSyAv-6b6y4qIILixgJhh1zoimOMc78M2RFk",   authDomain: "studio

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyDCelVT6SG5-vJvwNRQ3fpR4hiRC19Pj8E",
-  authDomain: "thambnail-downloader.firebaseapp.com",
-  projectId: "thambnail-downloader",
-  storageBucket: "thambnail-downloader.firebasestorage.app",
-  messagingSenderId: "22788421317",
-  appId: "1:22788421317:web:885e8e4e67fac383a622ac",
-  measurementId: "G-Q31YDWT76B"
+  apiKey: "AIzaSyAv-6b6y4qIILixgJhh1zoimOMc78M2RFk",
+  authDomain: "studio-2008852960-a9318.firebaseapp.com",
+  projectId: "studio-2008852960-a9318",
+  storageBucket: "studio-2008852960-a9318.appspot.com",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
