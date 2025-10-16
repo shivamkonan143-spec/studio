ad875a4e27bc0c2b081e932b8d7b30af57c2363a
Shivam Pandey <shivamkonan143@gmail.com>
2025-10-16 08:34:45 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,13 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // Important: return the modified config
+    return config
+  },
 };
 
 export default nextConfig;
