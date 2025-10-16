7caaa7d7dddb5842b03e8f3e79f5b0052f5042fb
Shivam Pandey <shivamkonan143@gmail.com>
2025-10-16 17:17:47 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid: e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { ThemeProvider } from '@/app/components/theme-provider';
 import { LanguageProvider } from '@/app/context/language-context';
-import { FirebaseClientProvider } from '@/firebase/provider';
+import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
   title: 'Thumbnail Downloader',
