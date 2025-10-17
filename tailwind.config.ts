0e5660fd3d2f132d3a81ff59e0ad75b4b6409030
Shivam Pandey <shivamkonan143@gmail.com>
2025-10-17 18:30:40 +0000

    Ager Manu button per Tap kare to Lage ki koi left side me hand se rope s

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-
 import type {Config} from 'tailwindcss';
 
 export default {
@@ -94,15 +93,20 @@ export default {
             transform: 'translateY(0)',
           },
         },
+        'pull-in-from-left': {
+          '0%': { transform: 'translateX(-100%)' },
+          '60%': { transform: 'translateX(5%)' },
+          '80%': { transform: 'translateX(-2%)' },
+          '100%': { transform: 'translateX(0)' },
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
+        'pull-in-from-left': 'pull-in-from-left 0.5s ease-out',
       },
     },
   },
   plugins: [require('tailwindcss-animate')],
 } satisfies Config;
-
-    
