a7afb9e9f2405f3f6adfebbbb247a2a1bffa3e7a
Shivam Pandey <shivamkonan143@gmail.com>
2025-10-17 20:28:22 +0000

    Menu section me jo chhota sa hole tha use delete karo

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -65,7 +65,6 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
-      <div className="sheet-hole"></div>
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
